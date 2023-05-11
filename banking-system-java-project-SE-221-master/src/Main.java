b490aba1a9643667f7224e510fc58c01eac89998
Monir Ullah <mullah725@gmail.com>
2023-05-11 10:57:47 +0600

    proper comment added

--- banking-system-java-project-SE-221-master/src/Main.java
+++ banking-system-java-project-SE-221-master/src/Main.java
@@ -1,44 +1,53 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-
-        import javax.swing.*;
-        import java.awt.*;
-        import java.awt.event.ActionEvent;
-        import java.awt.event.ActionListener;
-
+// Main class
 public class Main {
     public static void main(String[] args) {
+        // Create a new Login object
         Login login = new Login();
+        // Create a new JFrame
         JFrame frame = new JFrame("Banking System");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 300);
 
+        // Create a new JPanel
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         frame.add(panel);
 
+        // Create a Register button
         JButton registerButton = new JButton("Register");
         registerButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                // Prompt the user to enter their information
                 String name = JOptionPane.showInputDialog(frame, "Enter your name:");
                 String user = JOptionPane.showInputDialog(frame, "Enter Username:");
                 String pass = JOptionPane.showInputDialog(frame, "Enter Password:");
+                // Register the user
                 login.register(frame, name, user, pass);
             }
         });
         panel.add(registerButton);
 
+        // Create a Login button
         JButton loginButton = new JButton("Login");
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                // Prompt the user to enter their username and password
                 String user = JOptionPane.showInputDialog(frame, "Enter username:");
                 String pass = JOptionPane.showInputDialog(frame, "Enter Password:");
+                // Login the user
                 login.login(frame, user, pass);
             }
         });
         panel.add(loginButton);
 
+        // Create an Exit button
         JButton exitButton = new JButton("Exit");
         exitButton.addActionListener(new ActionListener() {
             @Override
--- banking-system-java-project-SE-221-master/src/UserInfo.java
+++ banking-system-java-project-SE-221-master/src/UserInfo.java
@@ -1,20 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-
-        import javax.swing.*;
-        import java.awt.*;
-        import java.awt.event.ActionEvent;
-        import java.awt.event.ActionListener;
-        import java.util.ArrayList;
-
+// UserInfo class
 public class UserInfo {
+    // ArrayList to store user balance information
     ArrayList<Integer> amount1 = new ArrayList<>();
 
+    // Method to create a new account for a user
     void new_account(JFrame frame, String name, int serial) {
+        // Display a welcome message
         JOptionPane.showMessageDialog(frame, "Welcome " + name);
         int size = amount1.size();
+        // Check if the user has an existing balance
         if (serial >= size) {
+            // Offer the user a free deposit
             int choice = JOptionPane.showConfirmDialog(frame, "You have 0 Balance. Would you like to get 100 tk Free deposit?", "Free Deposit", JOptionPane.YES_NO_OPTION);
             if (choice == JOptionPane.YES_OPTION) {
+                // Add the free deposit to the user's balance
                 amount1.add(100);
                 JOptionPane.showMessageDialog(frame, "Your Balance Is 100 tk.");
                 menu(frame, serial);
@@ -26,6 +31,7 @@ public class UserInfo {
         }
     }
 
+    // Method to display the menu options to the user
     void menu(JFrame frame, int serial) {
         while (true) {
             Object[] options = {"Deposit", "View Balance", "Withdraw", "Exit"};
@@ -46,25 +52,36 @@ public class UserInfo {
         }
     }
 
+    // Method to deposit money into the user's account
     void deposit(JFrame frame, int serial) {
+        // Prompt the user to enter the amount to deposit
         String amountStr = JOptionPane.showInputDialog(frame, "Enter the amount to deposit:");
         int amount = Integer.parseInt(amountStr);
+        // Display a message indicating successful deposit
         JOptionPane.showMessageDialog(frame, "Amount added " + amount);
+        // Update the user's balance
         amount += amount1.get(serial);
         JOptionPane.showMessageDialog(frame, "Your current amount is " + amount);
         amount1.set(serial, amount);
     }
 
+    // Method to view the user's current balance
     void view_balance(JFrame frame, int serial) {
         JOptionPane.showMessageDialog(frame, "Your Balance Is: " + amount1.get(serial));
     }
 
+    // Method to withdraw money from the user's account
     void withdraw(JFrame frame, int serial) {
+        // Display the user's current balance
         JOptionPane.showMessageDialog(frame, "Your Balance Is: " + amount1.get(serial));
+        // Prompt the user to enter the amount to withdraw
         String amount2Str = JOptionPane.showInputDialog(frame, "Enter The amount You want to Withdraw:");
         int amount2 = Integer.parseInt(amount2Str);
+        // Check if the user has enough money to withdraw
         if (100 < amount1.get(serial) - amount2) {
+            // Display a message indicating successful withdrawal
             JOptionPane.showMessageDialog(frame, "Withdrawn Amount: " + amount2);
+            // Update the user's balance
             amount2 = amount1.get(serial) - amount2;
             JOptionPane.showMessageDialog(frame, "Your new amount is " + amount2);
             amount1.set(serial, amount2);
