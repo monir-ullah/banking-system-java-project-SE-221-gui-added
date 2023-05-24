c72c45bd8de7bf3f358d2a9f1459fd12ae177a39
Monir Ullah <mullah725@gmail.com>
2023-05-25 02:24:29 +0600

    update button color

--- banking-system-java-project-SE-221-master/src/Main.java
+++ banking-system-java-project-SE-221-master/src/Main.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.RoundRectangle2D;
 
 // Main class
 public class Main {
@@ -14,12 +15,27 @@ public class Main {
         frame.setSize(400, 300);
 
         // Create a new JPanel
-        JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
         frame.add(panel);
 
         // Create a Register button
-        JButton registerButton = new JButton("Register");
+        JButton registerButton = new JButton("Register") {
+            @Override
+            protected void paintComponent(Graphics g) {
+                Graphics2D g2d = (Graphics2D) g.create();
+                g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                g2d.setColor(Color.decode("#1B9C85"));
+                g2d.fill(new RoundRectangle2D.Float(0, 0, getWidth() - 1, getHeight() - 1, 15, 15));
+                super.paintComponent(g2d);
+            }
+        };
+        registerButton.setForeground(Color.WHITE);
+        registerButton.setOpaque(false);
+        registerButton.setBorderPainted(false);
+        registerButton.setFocusPainted(false);
+        registerButton.setContentAreaFilled(false);
+        registerButton.setMargin(new Insets(10, 20, 10, 20));
         registerButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -31,10 +47,30 @@ public class Main {
                 login.register(frame, name, user, pass);
             }
         });
-        panel.add(registerButton);
+        c.gridx = 0;
+        c.gridy = 0;
+        c.insets = new Insets(10, 10, 10, 10);
+        panel.add(registerButton, c);
+
+
 
         // Create a Login button
-        JButton loginButton = new JButton("Login");
+        JButton loginButton = new JButton("Login") {
+            @Override
+            protected void paintComponent(Graphics g) {
+                Graphics2D g2d = (Graphics2D) g.create();
+                g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                g2d.setColor(Color.decode("#1B9C85"));
+                g2d.fill(new RoundRectangle2D.Float(0, 0, getWidth() - 1, getHeight() - 1, 15, 15));
+                super.paintComponent(g2d);
+            }
+        };
+        loginButton.setForeground(Color.WHITE);
+        loginButton.setOpaque(false);
+        loginButton.setBorderPainted(false);
+        loginButton.setFocusPainted(false);
+        loginButton.setContentAreaFilled(false);
+        loginButton.setMargin(new Insets(10, 20, 10, 20));
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -45,17 +81,37 @@ public class Main {
                 login.login(frame, user, pass);
             }
         });
-        panel.add(loginButton);
+        c.gridx = 1;
+        c.gridy = 0;
+        panel.add(loginButton, c);
 
         // Create an Exit button
-        JButton exitButton = new JButton("Exit");
+        JButton exitButton = new JButton("Exit") {
+            @Override
+            protected void paintComponent(Graphics g) {
+                Graphics2D g2d = (Graphics2D) g.create();
+                g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                g2d.setColor(Color.decode("#FF6347"));
+                g2d.fill(new RoundRectangle2D.Float(0, 0, getWidth() - 1, getHeight() - 1, 15, 15));
+                super.paintComponent(g2d);
+            }
+        };
+        exitButton.setForeground(Color.WHITE);
+        exitButton.setOpaque(false);
+        exitButton.setBorderPainted(false);
+        exitButton.setFocusPainted(false);
+        exitButton.setContentAreaFilled(false);
+        exitButton.setMargin(new Insets(10, 20, 10, 20));
         exitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.exit(0);
             }
         });
-        panel.add(exitButton);
+        c.gridx = 0;
+        c.gridy = 1;
+        c.gridwidth = 2;
+        panel.add(exitButton, c);
 
         frame.setVisible(true);
     }
